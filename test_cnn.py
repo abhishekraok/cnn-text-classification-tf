98ae8669032e007e3913d3ddc03795a3da6db9c7
Abhishek Rao <arao@microsoft.com>
2017-10-16 14:36:59 -0700

    Test training with actual data

--- test_cnn.py
+++ test_cnn.py
@@ -4,7 +4,8 @@ from tensorflow.contrib import learn
 import numpy as np
 import os
 import shutil
-from data_helpers import load_from_tsv
+from data_helpers import load_from_tsv, load_data_and_labels
+from sklearn.model_selection import train_test_split
 
 
 class TestCNN(TestCase):
@@ -14,6 +15,19 @@ class TestCNN(TestCase):
         vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length=100)
         x_train = x_dev = np.array(list(vocab_processor.fit_transform(x_text)))
         y_train = y_dev = np.array([[0, 1], [1, 0]])
+        train_cnn(flags=options, x_train=x_train, y_train=y_train, x_dev=x_dev, y_dev=y_dev,
+                  vocab_processor=vocab_processor)
+        cleanup()
+
+    # Long test
+    def test_cnn_polarity_good_metrics(self):
+        options = TrainingFlags()
+        polarity_positive_filename = 'data/rt-polaritydata/rt-polarity.pos'
+        polartity_negative_filename = 'data/rt-polaritydata/rt-polarity.pos'
+        x_text, y = load_data_and_labels(polarity_positive_filename, polartity_negative_filename)
+        vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length=100)
+        x = np.array(list(vocab_processor.fit_transform(x_text)))
+        x_train, x_dev, y_train, y_dev = train_test_split(x, y)
         train_cnn(flags=options, x_train=x_train, y_train=y_train, x_dev=x_dev, y_dev=y_dev,
                   vocab_processor=vocab_processor)
         cleanup()
@@ -44,3 +58,5 @@ def cleanup():
     default_summaries_foldername = '/summaries/*'
     if os.path.isdir(default_summaries_foldername):
         shutil.rmtree(default_summaries_foldername, ignore_errors=True)
+    if os.path.isdir('checkpoints'):
+        shutil.rmtree('checkpoints', ignore_errors=True)
